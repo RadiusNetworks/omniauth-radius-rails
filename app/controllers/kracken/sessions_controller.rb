3d6a4c9fa7462908954307f97d13a301f0aecf0a
Christopher Sexton <github@codeography.com>
2013-08-27 17:10:05 -0400

    Render sorry message on failure.

--- app/controllers/kracken/sessions_controller.rb
+++ app/controllers/kracken/sessions_controller.rb
@@ -13,11 +13,11 @@ module Kracken
     end
 
     def failure
+      render text: "Sorry, but you didn't allow access to our app!"
     end
 
     def index
       authenticate_user!
-      render text: 'hi'
     end
 
     protected
