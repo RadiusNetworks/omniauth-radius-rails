4a706673b39302302928afa31f1644f5862f39dd
Christopher Sexton <github@codeography.com>
2015-07-09 15:56:41 -0400

    Move exceptions to the top level of the module

--- lib/kracken/error.rb
+++ lib/kracken/error.rb
@@ -2,26 +2,23 @@ module Kracken
   KrackenError        = Class.new(StandardError)
   RequestError        = Class.new(KrackenError)
   MissingUIDError     = Class.new(KrackenError)
+  UnprocessableEntity = Class.new(KrackenError)
 
-  module Controllers
-    UnprocessableEntity = Class.new(StandardError)
-
-    class TokenUnauthorized < KrackenError
-      def initialize(msg = nil)
-        msg ||= 'HTTP Token: Access denied.'
-        super(msg)
-      end
+  class TokenUnauthorized < KrackenError
+    def initialize(msg = nil)
+      msg ||= 'HTTP Token: Access denied.'
+      super(msg)
     end
+  end
 
-    class ResourceNotFound < KrackenError
-      attr_reader :missing_ids, :resource
-      def initialize(resource, missing_ids)
-        @missing_ids = Array(missing_ids)
-        @resource    = resource
-        super(
-          "Couldn't find #{resource} with id(s): #{missing_ids.join(', ')}"
-        )
-      end
+  class ResourceNotFound < KrackenError
+    attr_reader :missing_ids, :resource
+    def initialize(resource, missing_ids)
+      @missing_ids = Array(missing_ids)
+      @resource    = resource
+      super(
+        "Couldn't find #{resource} with id(s): #{missing_ids.join(', ')}"
+      )
     end
   end
 end
