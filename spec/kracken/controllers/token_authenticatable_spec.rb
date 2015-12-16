131a0d09d1b50d2dc2ababd9d26bff88d7f5dbfb
Aaron Kromer <aaron.kromer@gmail.com>
2015-12-16 11:30:22 -0500

    Brain dump cache related behavior
    This is the spec outline for how I believe the new caching layer should
    behave. From this we can see there is some overlap in behavior across
    the states. Additionally, it seems we may have some slight adjustments
    to the existing behavior. I believe we can prove that everything is
    backwards compatible though, so it should not be an issue.

--- spec/kracken/controllers/token_authenticatable_spec.rb
+++ spec/kracken/controllers/token_authenticatable_spec.rb
@@ -4,7 +4,13 @@ module Kracken
   class TokenAuthController < BaseControllerDouble
     include Kracken::Controllers::TokenAuthenticatable
     public :authenticate_user_with_token!
-    public :current_user
+    # The module includes things as private so that they are not accidentally
+    # exposed as controller routes. However, we really treat some of them as the
+    # "public" API for the module:
+    public :current_auth_info,
+           :current_team_ids,
+           :current_user,
+           :current_user_id
 
     def authenticate_or_request_with_http_token(realm = nil)
       /\AToken token="(?<token>.*)"\z/ =~ request.env['HTTP_AUTHORIZATION']
@@ -14,10 +20,35 @@ module Kracken
 
   RSpec.describe Controllers::TokenAuthenticatable do
     describe "authenticating via a token" do
+      context "on a cache hit" do
+        it "munges the request headers to support parameterized tokens"
+        it "leaves the request header unchange when with no parameterized token"
+        it "uses the exising cache to bypass the authentication process"
+        it "returns the auth info"
+        it "exposes the auth info via the `current_` helpers"
+        it "lazy loads the current user"
+      end
+
+      context "on a cache miss with an invalid token" do
+        it "munges the request headers to support parameterized tokens"
+        it "leaves the request header unchange when with no parameterized token"
+        it "follows the token authentication process"
+        it "returns nil"
+        it "doesn't cache invalid tokens"
+      end
+
       context "on a cache miss with a valid token" do
         before do
           allow(Authenticator).to receive(:user_with_token)
         end
+
+        it "follows the token authentication process"
+        it "returns the auth info"
+        it "exposes the auth info via the `current_` helpers"
+        it "sets the auth info as the cache value"
+        it "sets the cache expiration to one minute by default"
+        it "sets the cache expiration to the environment setting `KRACKEN_TOKEN_TTL` when available"
+        it "eager loads the current user"
 
         it "munges the request headers to support parameterized tokens" do
           controller = TokenAuthController.new
